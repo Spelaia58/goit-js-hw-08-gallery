a74f5baac58213ecfe8899d1f4322b3285ab3117
spelaia58 <spelaia58@gmail.com>
2020-03-16 15:42:41 +0200

    w8

---
+++ js/task-1.js
@@ -0,0 +1,29 @@
+import galleryItems from "./gallery-items.js";
+
+const createImagesCard = galleryItem => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  const linkItem = document.createElement("a");
+  linkItem.classList.add("gallery__link");
+  linkItem.setAttribute('href', '');
+
+  const imgItem = document.createElement("img");
+  imgItem.classList.add("gallery__image");
+  imgItem.setAttribute("src", "");
+  imgItem.setAttribute("data-source", "");
+  imgItem.setAttribute("alt", "");
+
+  listItem.appendChild(linkItem);
+  linkItem.appendChild(imgItem);
+  const cardsImage = document.querySelector('.gallery');
+  cardsImage.appendChild(listItem);
+
+  return listItem;
+};
+ 
+
+const imagesCards = galleryItems.map(galleryItem => createImagesCard(galleryItem));
+console.log(imagesCard);
+
+const openModalimg = document.querySelector('.js-gallery')

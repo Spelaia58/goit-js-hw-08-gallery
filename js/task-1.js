979b0dd5b6101441af90e579dfa59761fa82c532
spelaia58 <spelaia58@gmail.com>
2020-03-20 17:20:17 +0200

    w8

--- js/task-1.js
+++ js/task-1.js
@@ -6,27 +6,64 @@ const createImagesCard = galleryItem => {
 
   const linkItem = document.createElement("a");
   linkItem.classList.add("gallery__link");
-  linkItem.setAttribute('href', '');
+  linkItem.setAttribute("href", "");
 
   const imgItem = document.createElement("img");
   imgItem.classList.add("gallery__image");
-  imgItem.setAttribute("src", "");
-  imgItem.setAttribute("data-source", "");
-  imgItem.setAttribute("alt", "");
+  imgItem.setAttribute("src", galleryItem.preview);
+  imgItem.setAttribute("data-source", galleryItem.original);
+  imgItem.setAttribute("alt", galleryItem.description);
 
   listItem.appendChild(linkItem);
   linkItem.appendChild(imgItem);
-  const cardsImage = document.querySelector('.gallery');
+  const cardsImage = document.querySelector(".gallery");
   cardsImage.appendChild(listItem);
 
   return listItem;
 };
- 
 
-const imagesCards = galleryItems.map(galleryItem => createImagesCard(galleryItem));
+const imagesCards = galleryItems.map(galleryItem =>
+  createImagesCard(galleryItem)
+);
 console.log(imagesCards);
 
-const openModalImg = document.querySelector('.js-gallery');
-openModalImg.addEventListener('click', () => {
-  document.URL.classList.add('.lightbox')
+const refs = {
+  galleryList: document.querySelector("ul.js-gallery"),
+  lightbox: document.querySelector(".js-lightbox"),
+  btn: document.querySelector('[data-action = "close-modal"]'),
+  
+};
+
+refs.galleryList.addEventListener("click", handleGalleryClick);
+function handleGalleryClick(event) {
+  event.preventDefault();
+  const target = event.target;
+  if (target.nodeName !== "IMG") return;
+  setActiveLink(target);
+}
+
+function setActiveLink(nextActiveLink) {
+  const currentActiveLink = refs.galleryList.querySelector("img.active");
+  if (currentActiveLink) {
+    currentActiveLink.classList.remove("active");
+  }
+  nextActiveLink.classList.add("active");
+  console.log(setActiveLink);
+}
+
+const openModalImg = document.querySelector(".js-gallery");
+openModalImg.addEventListener("click", () => {
+  event.preventDefault();
+  
+  refs.lightbox.classList.add('is-open');
+  
+  
+  refs.lightbox.querySelector('.lightbox__image').src = event.target.dataset.source;
+  
 });
+const closeModalImg = document.querySelector(
+  'button[data-action="close-lightbox"]'
+);
+closeModalImg.addEventListener("click", () => {
+  refs.lightbox.classList.remove("is-open")
+})
